efc343a6000bbc97d7e72b7b6085e62ec004f022
Marcin Praczko <marcin.praczko@gmail.com>
2022-01-16 22:09:33 +0100

    Add init version for python3 venv

--- scripts/create-python3-venv.sh
+++ scripts/create-python3-venv.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash
+ #!/usr/bin/env bash
 
 PROJECT_ROOT="$(git rev-parse --show-toplevel)"
-VENV_NAME="venv3"
+VENV_NAME="venv/p35"
 
 echo "[+] Creating venv (python3)"
 if [[ ! -d "${PROJECT_ROOT}/${VENV_NAME}" ]]; then
@@ -14,10 +14,8 @@ echo "[+] Activate venv"
 source ${VENV_NAME}/bin/activate
 env | grep -i VIRTUAL
 
-# echo "[+] Upgrade PIP / Setuptools"
-# pip install -U pip setuptools
+echo "[+] Upgrade PIP / Setuptools"
+pip install -U pip setuptools
 
 echo "[+] Installing PIP packages"
 pip install -r requirements.txt
-
-# molecule init scenario --driver-name vagrant --verifier-name goss
